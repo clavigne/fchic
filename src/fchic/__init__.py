d2f22d6958a947e6990ed5f12142f24454e9cb66
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-04-15 10:14:43 -0400

    single deck finders

--- src/fchic/__init__.py
+++ src/fchic/__init__.py
@@ -6,9 +6,43 @@ from typing import Dict, List, TextIO, Union
 from pyparsing import ParseResults
 
 # module
-from .parser_definition import fchk
+from .parser_definition import deck, fchk
 
-FchkOut = Dict[str, Union[List[str], List[int], List[float]]]
+Deck = Union[List[str], List[int], List[float]]
+"""Type of a single deck from a fchk file."""
+
+FchkOut = Dict[str, Deck]
+"""Type of the output from parsing a full fchk file.."""
+
+
+def _to_deck(group: ParseResults) -> Deck:
+    """Parse a deck into a python list."""
+    result: Deck
+    if "size" in group:
+        N = int(group["size"])
+        if group["type"] == "C":
+            result = ["".join(group.value)]
+        elif group["type"] == "I":
+            result = [int("".join(k)) for k in group.value]
+            if len(result) != N:
+                raise RuntimeWarning(f"expected {N} integers, got {len(result)}")
+        elif group["type"] == "R":
+            result = [float("".join(k)) for k in group.value]
+            if len(result) != N:
+                raise RuntimeWarning(f"expected {N} reals, got {len(result)}")
+        else:
+            raise TypeError(f'{group["type"]} is not one of C, I or R')
+    else:
+        if group["type"] == "C":
+            result = ["".join(group.value)]
+        elif group["type"] == "I":
+            result = [int("".join(group.value))]
+        elif group["type"] == "R":
+            result = [float("".join(group.value))]
+        else:
+            raise TypeError(f'{group["type"]} is not one of C, I or R')
+
+    return result
 
 
 def _to_dict(inp: ParseResults) -> FchkOut:
@@ -22,31 +56,7 @@ def _to_dict(inp: ParseResults) -> FchkOut:
     decks = inp.decks
     for group in decks:
         key = "".join(group["key"])
-        if "size" in group:
-            N = int(group["size"])
-            if group["type"] == "C":
-                result[key] = ["".join(group.value)]
-            elif group["type"] == "I":
-                result[key] = [int("".join(k)) for k in group.value]
-                if len(result[key]) != N:
-                    raise RuntimeWarning(
-                        f"expected {N} integers, got {len(result[key])}"
-                    )
-            elif group["type"] == "R":
-                result[key] = [float("".join(k)) for k in group.value]
-                if len(result[key]) != N:
-                    raise RuntimeWarning(f"expected {N} reals, got {len(result[key])}")
-            else:
-                raise TypeError(f'{group["type"]} is not one of C, I or R')
-        else:
-            if group["type"] == "C":
-                result[key] = ["".join(group.value)]
-            elif group["type"] == "I":
-                result[key] = [int("".join(group.value))]
-            elif group["type"] == "R":
-                result[key] = [float("".join(group.value))]
-            else:
-                raise TypeError(f'{group["type"]} is not one of C, I or R')
+        result[key] = _to_deck(group)
 
     return result
 
@@ -59,3 +69,38 @@ def loads(inp: str) -> FchkOut:
 def load(inp: TextIO) -> FchkOut:
     """Parse formatted checkpoint file from a filelike object."""
     return _to_dict(fchk.parseString(inp.read()))
+
+
+def deck_loads(inp: str, name: str) -> Deck:
+    """Parse single deck from a formatted checkpoint in string."""
+    where = inp.find(name)
+
+    if where < 0:
+        raise AttributeError(f"{name} could not be found in input string.")
+
+    group = deck.parseString(inp[where:])[0]
+    my_name = "".join(group["key"])
+    if my_name.strip() != name.strip():
+        raise RuntimeWarning(f"expected deck {name}, but got a deck named {my_name}")
+    return _to_deck(group)
+
+
+def deck_load(inp: TextIO, name: str) -> Deck:
+    """Parse single deck from a formatted checkpoint in a filelike object."""
+    start_pos = inp.tell()
+    for line in inp:
+        if name in line:
+            break
+    else:
+        inp.seek(start_pos)  # return to start pos
+        raise AttributeError(f"{name} could not be found in input string.")
+
+    rest = line + "\n" + inp.read()  # read rest of file
+    inp.seek(start_pos)  # return to start_pos
+
+    # parse rest
+    group = deck.parseString(rest)[0]
+    my_name = "".join(group["key"])
+    if my_name.strip() != name.strip():
+        raise RuntimeWarning(f"expected deck {name}, but got a deck named {my_name}")
+    return _to_deck(group)
--- tests/test_class.py
+++ tests/test_class.py
@@ -32,3 +32,36 @@ def test_read_file() -> None:
         result = fchic.load(f)
 
     assert len(result["Alpha Orbital Energies"]) == 102
+
+
+def test_read_deck() -> None:
+    """Test reading a single deck from a fchk string."""
+    ex_full = """TD-DFT calculation                                                      
+Freq      RB3LYP                                                      6-31G(d)            
+Number of atoms                            I               12
+Info1-9                                    I   N=           9
+           7           6           0           0           0         100
+           6          18        -502
+Multiplicity                               I                1
+Number of electrons                        I               42
+Number of alpha electrons                  I               21
+Route                                      C   N=           7
+# Geom=AllCheck Guess=Read SCRF=Check GenChk B3LYP/6-31G(d) 
+Symm=None Freq=(NoRaman)
+Nuclear charges                            R   N=          12
+  6.00000000E+00  6.00000000E+00  6.00000000E+00  6.00000000E+00  6.00000000E+00
+  6.00000000E+00  1.00000000E+00  1.00000000E+00  1.00000000E+00  1.00000000E+00
+  1.00000000E+00  1.00000000E+00
+"""
+    result = fchic.deck_loads(ex_full, "Multiplicity")
+    assert result == [1]
+    result = fchic.deck_loads(ex_full, "Nuclear charges")
+    assert len(result) == 12
+    assert result[0] == 6.0
+
+
+def test_read_deck_file() -> None:
+    """Test reading a single deck from a fchk file."""
+    with open("tests/data/data.fchk", "r") as f:
+        result = fchic.deck_load(f, "Cartesian Force Constants")
+    assert len(result) == 666  # number of the beast!
